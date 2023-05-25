48327363bacee1f9ed29d19ba35f08bb059b27b9
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-05-25 05:46:17 -0700

    0

---
+++ simple_shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+/**
+ * main - the main func
+ * @ac: ar count
+ * @av: ar vector 
+ * Return: is 0
+ */
+int main(int ac, char **av)
+{
+	void(ac);
+	void(av);
+
+	return(0);
+}
